cce5e9bfdb0425de66d4f8caa58cab7849306e97
Carlos <carlosacorti@hotmail.com>
2020-10-05 19:19:29 +0200

    serving satic files from cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -15,15 +15,40 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).then(res => {
-      if(res.status==404){
-        console.log('not found');
-        return fetch('imgs/dr-evil.gif');
-      }
-      return res;
-    }).catch((err)=>{
-      console.log(`not network: ${err}`);
-      return new Response('cant make the request');
+    caches.match(event.request).then(res=>{
+      return res ? res : fetch(event.request);
     })
+    // caches.open('wittr-static-v1')
+    //   .then(cache=>cache.keys())
+    //   .then(keys=>{
+    //     if (keys.map(key=>key.url).includes(event.request.url)) {
+    //     // if (keys.includes(event.request)) {
+    //       console.log('from cache')
+    //       return caches.match(event.request);
+    //     }
+    //     return fetch(event.request);
+    //   })
   );
+
+
+
+  
+  // event.respondWith(
+  //   caches.match(event.request)
+
+
+    // caches.open('wittr-static-v1').then(cache=>cache.keys()).then(keys=>fetch(keys[0].url))
+    
+    
+    // fetch(event.request).then(res => {
+    //   if(res.status==404){
+    //     console.log('not found');
+    //     return fetch('imgs/dr-evil.gif');
+    //   }
+    //   return res;
+    // }).catch((err)=>{
+    //   console.log(`not network: ${err}`);
+    //   return new Response('cant make the request');
+    // })
+  // );
 });
