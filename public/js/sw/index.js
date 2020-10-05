afd0582fe7c2f84c0b5a32d983734a738a5eeceb
Carlos <carlosacorti@hotmail.com>
2020-10-05 15:10:58 +0200

    fetching gif image on 404 status

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,12 +1,37 @@
 self.addEventListener('fetch', (event) => {
-  // console.log(event.request);
-  // console.log('just hello hello¡¡:):)');
-  const urlEnd = event.request.url.split('.').pop();
-  console.log(urlEnd);
-  if (urlEnd == 'jpg') {
-    console.log('si')
-    event.respondWith(
-      fetch('imgs/dr-evil.gif')
-    );
-  }
+  event.respondWith(
+    fetch(event.request).then(res => {
+      console.log(res.status);
+      if(res.status==404){
+        console.log('not found');
+        return fetch('imgs/dr-evil.gif');
+        // return fetch('imgs/dr-evil.gif').then(gif => {
+        //   return new Response(gif.body, { 
+        //       headers: {
+        //         'content-type': 'image/gif'
+        //       }
+        //     }
+        //   );
+        // });
+      }
+      return res;
+    }).catch((err)=>{
+      console.log(`not network: ${err}`);
+      return new Response('cant make the request');
+    })
+  );
 });
+
+// self.addEventListener('fetch', async (event) => {
+//   const res = await fetch(event.request);
+//   if(res.status==404){
+//     try {
+//       const gitFetch = await fetch('imgs/dr-evil.gif');
+//       return event.respondWith(new Response(gitFetch.body));
+//     } 
+//     catch (err) {
+//       console.log(`not network: ${err}`);
+//       return event.respondWith(new Response('cant make the request'));
+//     }
+//   }
+// });
