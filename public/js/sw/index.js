a16e1ca76a4f88dbe7ff131685bd3c0401b12562
Carlos <carlosacorti@hotmail.com>
2020-10-05 13:34:40 +0200

    custom response with service worker

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,5 +1,11 @@
 self.addEventListener('fetch', (event) => {
   // console.log(event.request);
-  console.log('just hello hello¡¡:)--');
+  // console.log('just hello hello¡¡:):)');
+  event.respondWith(new Response('Hello <b class=a-winner-is-me>world</b>', {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      }
+    })
+  );
 
 });
